84e79854fd12a0293786228d2e9017915d470bc9
Le-FireStarter <berriesrcute@gmail.com>
2022-01-25 22:46:35 +0100

    Trying different modifications to the dockerfile and the python file - pipeline just to see how changes affect the results

--- week-1-practice/docker_sql/pipeline.py
+++ week-1-practice/docker_sql/pipeline.py
@@ -1,5 +1,11 @@
+import sys
+
 import pandas as pd
+
+print (sys.argv)
+
+day = sys.argv[1]
 
 #trying what I've learned with this basic code.
 
-print ('Job finished without any flags')
+print (f'Job finished without any flags for day = {day}')
